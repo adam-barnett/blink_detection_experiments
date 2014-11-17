080d609f17efe2815d31ef1cd30b9dbc9c7584ed
adam-barnett <adam.barnett23@gmail.com>
2014-11-17 17:31:11 -0500

    Added a new script which will capture the entire webcam image, for when the eyes aren't being detected (i.e. when wearing glasses or when the light is uneven

---
+++ saving_eyes_to_file/saving_full_img_to_file.py
@@ -0,0 +1,24 @@
+import cv2
+
+
+video_src = 0
+cam = cv2.VideoCapture(video_src)
+saved = 0
+
+print 'Press space bar to save image displayed'
+print 'Press escape to exit'
+
+while True:
+    ret, img = cam.read()
+    if ret:
+      cv2.imshow('current_image', img)
+      key_press = cv2.waitKey(20)
+      if key_press == 32:
+        print 'saving image'
+        cv2.imwrite('image%d.png' % saved, img)
+        saved += 1
+      elif key_press == 27:
+        print 'closing'
+        cv2.destroyAllWindows()
+        break
+                              
